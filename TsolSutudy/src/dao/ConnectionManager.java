101bfb38d9c354ba8e3a680354341685424aedb4
DESKTOP-GQBACAP\seita <seita.1224.ds@outlook.com>
2017-05-20 21:40:02 +0900

    ConnectionManagerクラスにConnection取得メソッド(getConnection)を追加

--- TsolSutudy/src/dao/ConnectionManager.java
+++ TsolSutudy/src/dao/ConnectionManager.java
@@ -1,7 +1,22 @@
 package dao;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager{
+	private String url;
+	private String user;
+	private String passWord;
 
+	private ConnectionManager(String url,String user,String passWord){
+		this.url = url;
+		this.user = user;
+		this.passWord = passWord;
+	}
+
+	public Connection getConnection() throws SQLException{
+		Connection con = DriverManager.getConnection(url,user,passWord);
+		return con;
+	}
 }
